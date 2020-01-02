b305d29b5da09d8f5891f9272870da7243401f89
bashbash <amjad.bashiti@hotmail.com>
2020-01-02 13:46:11 +0200

    add ogranization profail model

--- Backend/src/finder/models.py
+++ Backend/src/finder/models.py
@@ -3,8 +3,37 @@ from django.db import models
 # Create your models here.
 
 
-class Finder(models.Model):
-    legalName = models.TextField()
+# class Finder(models.Model):
+#     legalName = models.TextField()
 
-    def __str__(self):
-        return self.legalName
+#     def __str__(self):
+#         return self.legalName
+
+class OrganizationProfile(models.Model):
+    pic = models.IntegerField(unique=True)
+    legalName = models.CharField(max_length=200)
+    businessName = models.CharField(max_length=200)
+    classificationType = models.CharField(max_length=50)
+    # numberOfProjects = models.IntegerField()
+    description = models.TextField()
+    address = models.TextField()
+    # collaborations = models.TextField()
+    tagsAndKeywords = models.TextField()
+
+    def getAddress(self):
+        return json.loads(self.address)
+
+    def setAddress(self, address):
+        self.address = json.dumps(address)
+
+    # def getCollaborations(self):
+    #     return json.loads(self.collaborations)
+
+    # def setCollaborations(self, collaborations):
+    #     self.collaborations = json.dumps(collaborations)
+
+    def getTagsAndKeywords(self):
+        return json.loads(self.tagsAndKeywords)
+
+    def setTagsAndKeywords(self, tagsAndKeywords):
+        self.tagsAndKeywords = json.dumps(tagsAndKeywords)

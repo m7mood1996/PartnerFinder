504a25adb2648152de70ec25850028f20f1f7d7f
bashbash96 <amjad.bashiti.96@gmail.com>
2020-06-14 23:32:24 +0300

    Changed results table background and text colors, and refactored table icons import

--- Frontend/partner-finder/src/components/ResultsTable.js
+++ Frontend/partner-finder/src/components/ResultsTable.js
@@ -1,46 +1,14 @@
 import React from "react";
-import MaterialTable from "material-table";
-import { forwardRef } from "react";
+import MaterialTable, { MTableToolbar } from "material-table";
+import { TablePagination } from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
 
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import { tableIcons } from "../utils";
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-};
+const MyTablePagination = styled(TablePagination)((theme) => ({
+  color: "white",
+  fontSize: "16px",
+}));
 
 function ResultsTable(props) {
   const [data, setData] = React.useState(props.data);
@@ -65,9 +33,54 @@ function ResultsTable(props) {
       <div>
         {data && data.length === 0 ? null : (
           <MaterialTable
+            style={{ color: "white", backgroundColor: "#02203c" }}
             icons={tableIcons}
             title={title}
-            options={{ exportButton: true }}
+            components={{
+              Pagination: (props) => <MyTablePagination {...props} />,
+              Toolbar: (props) => (
+                <div
+                  style={{
+                    backgroundColor: "#02203c",
+                    color: "white",
+                  }}
+                >
+                  <MTableToolbar {...props} />
+                </div>
+              ),
+            }}
+            options={{
+              actionsCellStyle: {
+                backgroundColor: "#02203c",
+                textAlign: "center",
+                color: "white",
+              },
+              cellStyle: {
+                backgroundColor: "#02203c",
+                textAlign: "center",
+                color: "white",
+              },
+              headerStyle: {
+                backgroundColor: "#02203c",
+                textAlign: "center",
+                color: "white",
+              },
+              searchFieldStyle: {
+                backgroundColor: "#02203c",
+                textAlign: "center",
+                color: "white",
+              },
+              filterCellStyle: {
+                backgroundColor: "#02203c",
+                textAlign: "center",
+                color: "white",
+              },
+              showEmptyDataSourceMessage: false,
+              hideFilterIcons: true,
+              header: true,
+              exportButton: true,
+              exportAllData: true,
+            }}
             columns={columns}
             data={data}
             editable={{
--- Frontend/partner-finder/src/utils.js
+++ Frontend/partner-finder/src/utils.js
@@ -1,32 +1,166 @@
+import React, { forwardRef } from "react";
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
 const EU_columns = [
-    { title: "Name", field: "legalName" },
-    { title: "Type", field: "classificationType" },
-    { title: "Country", field: "country" },
-    { title: "Data Status", field: "dataStatus" },
-    { title: "Number of EU Projects", field: "numberOfProjects" },
-    { title: "Description", field: "description" },
-    { title: "Was Coordinator?", field: "consorsiumRoles", render: rowData => rowData.consorsiumRoles ? 'Yes' : 'No' }
+  { title: "Name", field: "legalName" },
+  { title: "Type", field: "classificationType" },
+  { title: "Country", field: "country" },
+  { title: "Data Status", field: "dataStatus" },
+  { title: "Number of EU Projects", field: "numberOfProjects" },
+  { title: "Description", field: "description" },
+  {
+    title: "Was Coordinator?",
+    field: "consorsiumRoles",
+    render: (rowData) => (rowData.consorsiumRoles ? "Yes" : "No"),
+  },
 ];
 
 const B2Match_columns = [
-    { title: "Name", field: "participant_name" },
-    { title: "Org. Name", field: "organization_name" },
-    { title: "Type", field: "org_type" },
-    { title: "Country", field: "address" },
-    { title: "Website", field: "org_url" },
-    { title: "Description", field: "description" },
+  { title: "Name", field: "participant_name" },
+  { title: "Org. Name", field: "organization_name" },
+  { title: "Type", field: "org_type" },
+  { title: "Country", field: "address" },
+  { title: "Website", field: "org_url" },
+  { title: "Description", field: "description" },
 ];
 
-const companyTypesOptions = [
-    "SME",
-    "International Organization",
-    "Higher or Secondary Education",
-    "Research Organization",
-    "Private for Profit Organization",
-    "Public Organization",
-    "Other",
-];
+const tableIcons = {
+  Add: forwardRef((props, ref) => (
+    <AddBox
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Check: forwardRef((props, ref) => (
+    <Check
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Clear: forwardRef((props, ref) => (
+    <Clear
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Delete: forwardRef((props, ref) => (
+    <DeleteOutline
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Edit: forwardRef((props, ref) => (
+    <Edit
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Export: forwardRef((props, ref) => (
+    <SaveAlt
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Filter: forwardRef((props, ref) => (
+    <FilterList
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  FirstPage: forwardRef((props, ref) => (
+    <FirstPage
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  LastPage: forwardRef((props, ref) => (
+    <LastPage
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  NextPage: forwardRef((props, ref) => (
+    <ChevronRight
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  ResetSearch: forwardRef((props, ref) => (
+    <Clear
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  Search: forwardRef((props, ref) => (
+    <Search
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  SortArrow: forwardRef((props, ref) => (
+    <ArrowDownward
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  ThirdStateCheck: forwardRef((props, ref) => (
+    <Remove
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+  ViewColumn: forwardRef((props, ref) => (
+    <ViewColumn
+      {...props}
+      ref={ref}
+      style={{ backgroundColor: "#02203c", color: "white" }}
+    />
+  )),
+};
 
-const BACKEND_URL = 'http://62.90.89.14:8000/api/'
+const BACKEND_URL = "http://62.90.89.14:8000/api/";
 
-export { EU_columns, B2Match_columns, companyTypesOptions, BACKEND_URL }
+export { EU_columns, B2Match_columns, BACKEND_URL, tableIcons };
